d9ba5e1732da69d2ca86277d1fedd12a3592d9b2
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-03-03 09:23:06 +0530

    okie

---
+++ FatArrowFun/five.js
@@ -0,0 +1,11 @@
+let add = (a, b) => a + b
+
+let r1 = add(10, 20)
+console.log(r1)
+
+//-----------
+function add1(a, b) {
+    return a + b
+}
+let r2 = add1(10, 20)
+console.log(r2)
---
+++ FatArrowFun/seven.js
@@ -0,0 +1,18 @@
+/* function wish() {
+    console.log("GM")
+}
+wish()
+
+function wish() {
+    console.log("GN")
+}
+wish() */
+
+var wish = () => {
+    console.log("GM")
+}
+wish()
+var wish = () => {
+    console.log("GN")
+}
+wish()
